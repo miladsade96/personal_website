a34b18f4e55fe6e14eade921cceb8f7332caadda
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-25 11:58:46 +0330

    Removing video element for tablet and mobile devices

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 
 const dynamicYear = new Date().getFullYear();
 document.querySelector(".year").textContent = String(dynamicYear);
+
+if (window.innerWidth <= 768) {
+  document.querySelector("#codingVideo").remove();
+}
