7041a89c199c81bb0e98a9a1e682a8b8a1c777a6
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-04-06 12:36:37 +0330

    Removing element deleter

--- script.js
+++ script.js
@@ -2,7 +2,3 @@
 
 const dynamicYear = new Date().getFullYear();
 document.querySelector(".year").textContent = String(dynamicYear);
-
-if (window.innerWidth <= 768) {
-  document.querySelector("#codingVideo").remove();
-}
